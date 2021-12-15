89fcd00026715bd61521bc58b4d2d687a5066976
karima <hda.karima@gmail.com>
2021-12-15 10:28:19 +0100

    sixth commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,22 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer
 {
+
+
+@Override
+5
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+6
+        return application.sources(App.class);
+7
+    }
     public static void main( String[] args )
     {
+     SpringApplication.run(App.class, args);
         System.out.println( "Hello World!" );
     }
 }
